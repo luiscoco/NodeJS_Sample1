e1a4a51e258f916eb53869ff622a96376a3ff4d3
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-25 10:57:57 +0200

    fixed the test result

--- test/sample.test.js
+++ test/sample.test.js
@@ -8,6 +8,6 @@ describe('Sample Test Suite', () => {
   });
 
   it('should fail', () => {
-    assert.strictEqual(1 + 1, 3); // This test will fail intentionally
+    assert.strictEqual(6 + 5, 11); // This test will fail intentionally
   });
 });
